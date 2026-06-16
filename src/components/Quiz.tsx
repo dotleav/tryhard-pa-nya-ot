a8ed7cbbe631604500a2dbc28fc6cf164534ed9b
dotleav <whyutakethisusername@gmail.com>
2026-06-16 13:01:11 +0700

    Update Quiz.tsx Fix ergonomic pengerjaan

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -359,7 +359,7 @@ export default function Quiz() {
                 PA Quiz
               </h1>
               <p style={{ color: '#6e7681', fontSize: '0.72rem', marginTop: '2px' }}>
-                Latihan Soal Kedokteran — Patologi Anatomi Endokrin
+                Latihan Soal Medis — Patologi Anatomi Endokrin
               </p>
             </div>
 
